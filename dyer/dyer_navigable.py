ba833f598cb0c1d1634c7c9f029328dd7580cabf
Konstantin Yegupov <yk4ever@gmail.com>
2010-08-04 00:06:44 +0100

    Navigable dictionary is now built by a single script

--- dyer/dyer_navigable.py
+++ dyer/dyer_navigable.py
@@ -1,5 +1,10 @@
+# coding: utf-8
+
 import json,codecs,re,math,os
 from StringIO import StringIO
+import glob
+from HTMLParser import HTMLParser 
+from htmlentitydefs import name2codepoint
 
 def distribute(accum):
     weights = [min(e[1],pagesize) for e in accum]
@@ -79,24 +84,182 @@ except:
     pass
     
     
-pagesize = 40
-letters_in_row = 9
-subdivs_in_row = 2
-sink_letters = StringIO() 
-        
+def parse_source(langletter):
+    articles = []
 
-letters = [chr(x) for x in range(ord('a'),ord('z')+1)]
-for li,letter in enumerate(letters):
-    print >>sink_letters, """<td>%s</td> """ % letter.upper(),
-    if li%letters_in_row == letters_in_row-1:
-        print >>sink_letters, """</tr><tr>"""  
+    re_w = re.compile("[a-zA-Z]*-?[a-zA-Z]+")
+
+    S_EXPECT_WORD = 0
+    S_READING_WORD = 1
+    S_READING_DEF = 3
+
+    strong = ["b","strong"]
+    em = ["i","em"]
+
+
+
+
+    class MyParser(HTMLParser):
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.state = 0
+            self.in_key = False
+            self.key = ""
+            self.baseword = ""
+            self.lastroot = ""
+            self.article = ""
+            self.lastword = ""
+            self.c = 0
+            self.is_new_entry = True
+            self.chain = []
+            self.in_lang_sources = False
+            
+        def handle_starttag(self, tag, attrs):
+            if tag=="p":
+                self.state+=1
+            if self.state == 2:
+                #~ if attrs:
+                    #~ print "oh boy, attrs!", attrs
+                    #~ raise
+                if tag in strong:
+                    self.in_key = True
+                if tag in em:
+                    self.chain.append("ex")
+                    self.article += "<ex>"
+                        
+                if tag=="br":
+                    if self.in_key:
+                        self.add_corrected_key()
+                    self.chain.reverse()
+                    for tn in self.chain:
+                        self.article += "</"+tn+">"
+                    self.save_article(True)
+                    
+                else:
+                    pass
+                    #~ self.chain.append(tag)
+            
+        def handle_endtag(self, tag):
+            if self.state == 2:
+                
+                if tag in strong:
+                    if self.in_key:
+                        self.add_corrected_key()
+                        self.in_key = False
+                if tag in em:
+                    assert self.chain.pop() == "ex"
+                    self.article += "</ex>"
+                    
+
+        
+        def handle_data(self, data):
+            if self.state == 2:
+                if self.in_lang_sources:
+                    return
+                if unichr(8212) in data:
+                    data = data.split(unichr(8212))[0].rstrip()        
+                    self.in_lang_sources = True
+                if self.in_key:
+                    self.key += data
+                else:
+                    self.article += data
+        
+        def handle_charref(self, name):
+            cpoint = int(name)
+            if cpoint>255:
+                self.handle_data(unichr(cpoint))
+            else:
+                self.handle_data(chr(cpoint).decode("cp1252"))
+
+        def handle_entityref(self, name):
+            self.handle_data(unichr(name2codepoint[name]))
+
+        def handle_startendtag(self, tag, attrs):
+            if self.state == 2:
+                if tag=="br":
+                    if self.in_key:
+                        self.add_corrected_key()
+                    self.chain.reverse()
+                    for tn in self.chain:
+                        self.article += "</"+tn+">"
+                    self.save_article(True)
+                #~ if attrs:
+                    #~ print "oh boy, attrs!", attrs
+                    #~ raise
+
+        def add_corrected_key(self):
+            self.key = self.key.replace(u"\n",u" ").strip()
+            keys = []
+            for k in self.key.split(u","):
+                words = k.strip().split(" ")
+                if self.baseword=="" and len(words)==1:
+                    self.baseword = words[0].split("-")[0]
+                words2 = []
+
+                for w in words:
+                    #~ words2.append(w)
+                    if w.startswith("-") and self.baseword!="":
+                        words2.append(self.baseword+w[1:].replace("-",""))
+                    else:
+                        words2.append(w.replace("-",""))
+                keys.append(" ".join(words2))
+            
+            wrapped = ", ".join(["<k>"+key.strip()+"</k>" for key in keys])
+            self.article += " "+wrapped+" "
+            self.key = ""
+            
+              
+        def save_article(self, end_of_entry):
+
+            articles.append(self.article.replace("  "," ").strip())
+                    
+            self.key = ""
+            self.article = ""
+            self.baseword = ""
+            self.chain = []
+            self.in_lang_sources = False
+
+            
+    for fn in glob.glob(langletter+"*.htm"):
+        print fn
+        p = MyParser()
+        p.feed(open(fn).read().decode('latin-1'))
+        
+        #~ soup = BeautifulSoup(open(fn).read())
+
+        #~ p = soup.findAll('p')[1]
+
+        #~ c.key = ""
+        #~ c.value = ""
+        #~ for child in p:
+            #~ process_node(child, c, toplevel = True)
+        #~ register_word(c)
+        #~ print fn, len(dic)
+
+    return articles    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+pagesize = 40
 
 subdivs_all = {}
     
 for langprefix in ["io","en"]:    
-    dictionary = json.load(codecs.open("dyer_%s.json" % langprefix, "rt", "utf-8"))
 
-    articles = dictionary["articles"]
+    articles = parse_source(langprefix[0])
 
     re_pureword = re.compile("[a-z]+")
 
@@ -105,10 +268,12 @@ for langprefix in ["io","en"]:
 
     print langprefix,len(articles)
     for i,a in enumerate(articles):
+        print a.strip()[:50]
         ar = a.strip()[3:]
         j = ar.index("<")
         ar = ar[:j].lower()
         mo = re_pureword.search(ar)
+
         titles.append( (mo.group(), i) )
         
     titles.sort(key=lambda x:x[0])
